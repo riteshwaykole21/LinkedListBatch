fc2d4bd1ec48f910889eacc1d1a35e98348663ee
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-23 22:09:05 +0530

    [Ritesh]add : Simple Linked list Add Three Number

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node( data);
+            node.data = data;
+            if(this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List Is Empty");
+            }
+            Console.WriteLine("Linked List is:  ");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            LinkedList linkedList = new LinkedList();
+            while (flag)
+            {
+                Console.WriteLine("1 Add");
+                Console.WriteLine("2: Display");
+                Console.WriteLine("Enter a Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 2:
+                        linkedList.Display();
+                            break;
+                    default: 
+                        Console.WriteLine("Please Enter Correct Option");
+                        break;
+                }
+            }
+        }
+    }
+}
